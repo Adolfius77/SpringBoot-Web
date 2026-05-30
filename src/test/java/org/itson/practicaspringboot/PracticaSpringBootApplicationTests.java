74e2cb4b63dc20c4de7562d643c2a6530d3b3be0
Adolfo Ortega <jose.ortega252882@potros.itson.edu.mx>
2026-05-30 16:02:17 -0700

    repaso spring boot

---
+++ src/test/java/org/itson/practicaspringboot/PracticaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.itson.practicaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
